453d4e885304e28d83ff8a24d2c1dfa49b9475c6
Stefano Lottini <lumidoro@gmail.com>
2016-09-07 09:17:27 +0200

    Morphed the logger into a (possibly remote) mongoDB logger

--- RemoteLogger/logger.py
+++ RemoteLogger/logger.py
@@ -1,24 +1,36 @@
+#!/usr/bin/env python
+
 import requests
 
 from time import sleep
 import json
 import sys
+from datetime import datetime
 
+from mongo_settings import gimme_db
+# it is expected that gimme_db returns a PyMongo database object. Implement it your way, with your auth and so on.
+
+collectionName='orsetti-readings'
+
+dateFormat='%d %b %Y %H:%M:%S'
+
+FieldConverters={
+        'date': lambda d: datetime.strptime(d,dateFormat),
+        'temp_int': float,
+        'temp_int_dig' : float,
+        'temp_ext': float,
+        'heatindex': float,
+        'humidity': float,
+    }
 
 def main():
     answers=[]
-    while True:
-        requrl='http://93.50.192.43/orsetti/api.php'
-        req=requests.get(requrl)
-        rj=req.json()
-        answers.append(rj)
-        print '*',
-        sys.stdout.flush()
-        sleep(60)
-        if len(answers)%1==0:
-            print '\nSaving... (%i)' % len(answers),
-            json.dump(answers,open('log.json','w'))
-            print 'Done'
+    loggerCollection=gimme_db(verbose=False)[collectionName]
+    requrl='http://93.50.192.43/orsetti/api.php'
+    req=requests.get(requrl)
+    Resp=req.json()
+    parsedResp={fld: conv(Resp[fld]) for fld,conv in FieldConverters.iteritems()}                
+    loggerCollection.insert_one(parsedResp)
 
 if __name__=='__main__':
     main()
