4e1d6902a0f467231d368bc82c71fe3066acc89d
Stefano Lottini <lumidoro@gmail.com>
2016-09-07 10:26:41 +0200

    Made the plotter mongo-aware and made its graphics better

--- RemoteLogger/orsettiplotter.py
+++ RemoteLogger/orsettiplotter.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 
-import matplotlib.pyplot as plt
-import pandas as pd
-import json
 
-FNAME='log.json'
+from datetime import datetime, timedelta
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from mongo_settings import gimme_db
+# it is expected that gimme_db returns a PyMongo database object. Implement it your way, with your auth and so on.
+
+collectionName='orsetti-readings'
 
 FIELDS=[
             {
@@ -18,18 +22,24 @@ FIELDS=[
             },
         ]
 
-def main():
-    # loading
-    data=json.load(open(FNAME))
-    df=pd.DataFrame(data)
+def main(daysBack=2):
+    # loading data from DB
+    loggerCollection=gimme_db(verbose=False)[collectionName]
+
+    _qry={'date':{'$gte': datetime.now()-timedelta(days=daysBack)}}
+    _prj={'_id':False}
+
+    df=pd.DataFrame(list(loggerCollection.find(_qry,_prj)))
     df=df.set_index('date')
     
     # plotting
     fig,ax=plt.subplots(len(FIELDS),1)
-    for fax,flist in zip(ax,FIELDS):
+    for fax,flist,xlegend,ylabel in zip(ax,FIELDS,[False,True],['Temperature (^C)','Rel. Humidity (%%)']):
         for fname,flabel in flist.iteritems():
-            df[fname].plot(label=flabel,ax=fax)
+            df[fname].plot(style='o',label=flabel,ax=fax)
         fax.legend()
+        fax.get_xaxis().set_visible(xlegend)
+        fax.set_ylabel(ylabel)
     plt.show()
 
 if __name__=='__main__':
